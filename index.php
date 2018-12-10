c62de341aa81a4bfc2edd361182132c4aa9e6082
verumnetworks <45750104+verumnetworks@users.noreply.github.com>
2018-12-09 22:10:53 -0800

    index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+
+$bucket = '*** Your Bucket Name ***';
+$keyname = '*** Your Object Key ***';
+                        
+$s3 = new S3Client([
+    'version' => 'latest',
+    'region'  => 'us-east-1'
+]);
+
+try {
+    // Upload data.
+    $result = $s3->putObject([
+        'Bucket' => $bucket,
+        'Key'    => $keyname,
+        'Body'   => 'Hello, world!',
+        'ACL'    => 'public-read'
+    ]);
+
+    // Print the URL to the object.
+    echo $result['ObjectURL'] . PHP_EOL;
+} catch (S3Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
